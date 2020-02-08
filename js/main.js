c68e4292779019ebb9a66fc53301c9fb0a42e395
vilmosan <vvebdevelopment@gmail.com>
2020-02-08 19:35:33 +0100

    Pushing local files to GitHub account.

--- js/main.js
+++ js/main.js
@@ -10,19 +10,6 @@
     }
   });
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
-
   // Initiate the wowjs animation library
   new WOW().init();
 
